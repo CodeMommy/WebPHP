27bf3f480736994e3a248fbbefdd46310a83f737
Candison November <kandisheng@163.com>
2015-12-08 22:43:16 +0800

    Vendor

---
+++ framework/vendor/hello/hello.php
@@ -0,0 +1,14 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | @author    Candison November (www.kandisheng.com)
+// | @location  Nanjing China
+// +----------------------------------------------------------------------
+
+class HelloWorld
+{
+    public function __construct()
+    {
+        echo 'Hello World';
+    }
+}
