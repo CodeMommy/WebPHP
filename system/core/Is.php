cd44fa25d85eb974746901458626412f8e437547
Candison November <kandisheng@163.com>
2016-08-18 14:57:06 +0800

    Is

---
+++ system/core/Is.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * @author   Candison November (www.kandisheng.com)
+ * @location Nanjing China
+ */
+
+namespace LuckyPHP;
+
+class Is
+{
+    public static function php()
+    {
+        return true;
+    }
+}
