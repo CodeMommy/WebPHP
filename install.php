5a205711272f70ff6a9fa1030056afe78127ca16
Candison <kandisheng@163.com>
2017-04-21 18:30:29 +0800

    Update

--- install.php
+++ install.php
@@ -1,4 +1,30 @@
 <?php
+
+/**
+ * CodeMommy WebPHP
+ * @author Candison November <www.kandisheng.com>
+ */
+
+/**
+ * @param $path
+ */
+function deleteDirectory($path)
+{
+    $directory = dir($path);
+    while (($item = $directory->read()) != false) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+        $file = $directory->path . '/' . $item;
+        if (is_dir($file)) {
+            deleteDirectory($file);
+            rmdir($file);
+        } else {
+            unlink($file);
+        }
+    }
+}
+
 if (!file_exists('application/environment.yaml')) {
     copy('application/environment.example.yaml', 'application/environment.yaml');
 }
@@ -6,3 +32,6 @@ unlink('composer.json');
 copy('composer.example.json', 'composer.json');
 unlink('composer.example.json');
 unlink('install.php');
+unlink('gulpfile.js');
+unlink('package.json');
+deleteDirectory('system');
