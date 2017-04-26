b103e5fc90b35321bd6f59d46ef5e7c00a9d133f
Candison <kandisheng@163.com>
2017-04-26 09:31:49 +0800

    Add CodeMommy/TaskPHP

--- task.php
+++ task.php
@@ -7,133 +7,95 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
+use CodeMommy\TaskPHP\Task;
 use Symfony\Component\Filesystem\Filesystem;
 
-Task::start();
-
-class Task
+/**
+ * Task Update Version
+ */
+function taskUpdateVersion()
 {
-    /**
-     * Get Parameter
-     *
-     * @param $number
-     * @param string $default
-     *
-     * @return string
-     */
-    private static function getParameter($number, $default = '')
-    {
-        if (isset($_SERVER['argv'][$number])) {
-            return $_SERVER['argv'][$number];
-        }
-        return $default;
-    }
+    $file = __DIR__ . '/composer.json';
+    $composer = file_get_contents($file);
+    $composer = json_decode($composer, true);
+    $version = $composer['version'];
+    $version = explode('.', $version);
+    $version[2] = intval($version[2]) + 1;
+    $version = implode('.', $version);
+    $composer['version'] = $version;
+    $composer = json_encode($composer, JSON_PRETTY_PRINT);
+    $composer = str_replace('\\/', '/', $composer);
+    file_put_contents($file, $composer);
+    echo(sprintf('Updated version to %s.', $version));
+}
 
-    /**
-     * Line
-     */
-    private static function line()
-    {
-        echo(sprintf('--------------------%s', PHP_EOL));
-    }
+Task::add('update-version', 'Update Version', 'taskUpdateVersion');
 
-    /**
-     * Start
-     */
-    public static function start()
-    {
-        $task = self::getParameter(1, 'Help');
-        $task = sprintf('task%s', $task);
-        self::$task();
+/**
+ * Task Clear
+ */
+function taskClear()
+{
+    $removeList = array();
+    array_push($removeList, __DIR__ . '/application/_runtime');
+    $fileSystem = new Filesystem();
+    foreach ($removeList as $value) {
+        $fileSystem->remove($value);
     }
+    echo(sprintf('Clear Finished.'));
+}
 
-    /**
-     * Default Task
-     */
-    protected static function taskHelp()
-    {
-        echo(sprintf('Tasks are:%s', PHP_EOL));
-        self::line();
-        $class = get_class_methods(__CLASS__);
-        sort($class);
-        foreach ($class as $key => $value) {
-            if (substr($value, 0, 4) == 'task') {
-                echo(sprintf('%s%s', substr($value, 4), PHP_EOL));
-            }
-        }
+Task::add('clear', 'Clear', 'taskClear');
+
+/**
+ * Task Run
+ */
+function taskRun()
+{
+    $port = intval(Task::getParameter(0, 80));
+    if ($port == 80) {
+        echo(sprintf('Visit http://localhost%s', PHP_EOL));
+    } else {
+        echo(sprintf('Visit http://localhost:%s%s', $port, PHP_EOL));
     }
+    system(sprintf('cd public && php -S 0.0.0.0:%s', $port));
+}
 
-    /**
-     * Task Update Version
-     */
-    protected static function taskUpdateVersion()
-    {
-        $file = __DIR__ . '/composer.json';
-        $composer = file_get_contents($file);
-        $composer = json_decode($composer, true);
-        $version = $composer['version'];
-        $version = explode('.', $version);
-        $version[2] = intval($version[2]) + 1;
-        $version = implode('.', $version);
-        $composer['version'] = $version;
-        $composer = json_encode($composer, JSON_PRETTY_PRINT);
-        $composer = str_replace('\\/', '/', $composer);
-        file_put_contents($file, $composer);
-        echo(sprintf('Updated version to %s.', $version));
-    }
+Task::add('run', 'Run', 'taskRun');
 
-    /**
-     * Task Clear
-     */
-    protected static function taskClear()
-    {
-        $removeList = array();
-        array_push($removeList, __DIR__ . '/application/_runtime');
-        $fileSystem = new Filesystem();
-        foreach ($removeList as $value) {
-            $fileSystem->remove($value);
-        }
-        echo(sprintf('Clear Finished.'));
-    }
+/**
+ * Task Update
+ */
+function taskUpdate()
+{
+    system('php -v');
+    Task::line();
+    system('git pull');
+    Task::line();
+    system('composer self-update');
+    Task::line();
+    system('composer update');
+}
 
-    /**
-     * Task Run
-     */
-    protected static function taskRun()
-    {
-        $port = intval(self::getParameter(2, 80));
-        if ($port == 80) {
-            echo(sprintf('Visit http://localhost%s', PHP_EOL));
-        } else {
-            echo(sprintf('Visit http://localhost:%s%s', $port, PHP_EOL));
-        }
-        system(sprintf('cd public && php -S 0.0.0.0:%s', $port));
-    }
+Task::add('update', 'Update Composer', 'taskUpdate');
 
-    /**
-     * Task Update
-     */
-    protected static function taskUpdate()
-    {
-        system('php -v');
-        self::line();
-        system('git pull');
-        self::line();
-        system('composer self-update');
-        self::line();
-        system('composer update');
-    }
-
-    /**
-     * Task Test
-     */
-    protected static function taskTest()
-    {
-        $test = self::getParameter(2);
-        if (empty($test)) {
-            system('phpunit');
-        } else {
-            system(sprintf('cd test && phpunit %sTest --repeat 100', $test));
-        }
+/**
+ * Task Test
+ */
+function taskTest()
+{
+    $test = Task::getParameter(0);
+    if (empty($test)) {
+        system('phpunit');
+    } else {
+        system(sprintf('cd test && phpunit %sTest --repeat 100', $test));
     }
 }
+
+Task::add('test', 'Test', 'taskTest');
+
+/**
+ * Start
+ */
+Task::config('CodeMommy WebPHP Task', '');
+Task::run();
