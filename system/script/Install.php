26c7d7a7a87059151b83e5f20607654cb3c0cfab
Candison <kandisheng@163.com>
2017-11-21 09:51:49 +0800

    0.0.22

--- system/script/Install.php
+++ system/script/Install.php
@@ -15,9 +15,9 @@ class Install
 {
     /**
      * Delete Directory
-     * @param $path
+     * @param string $path
      */
-    private static function deleteDirectory($path)
+    private static function deleteDirectory($path = '')
     {
         $directory = dir($path);
         while (($item = $directory->read()) !== false) {
@@ -36,16 +36,34 @@ class Install
     }
 
     /**
+     * Remove
+     * @param string $file
+     */
+    private static function remove($file = '')
+    {
+        if (is_dir($file)) {
+            self::deleteDirectory($file);
+        }
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+
+    /**
      * Start
      */
     public static function start()
     {
         copy('application/environment.example.yaml', 'application/environment.yaml');
         file_put_contents('composer.json', '{}');
-        unlink('.travis.yml');
-        unlink('phpunit.xml');
-        unlink('task.php');
-        deleteDirectory('system');
-        deleteDirectory('test');
+        $removeList = array(
+            'system',
+            'test',
+            '.travis.yml',
+            'phpunit.xml'
+        );
+        foreach ($removeList as $file) {
+            self::remove($file);
+        }
     }
 }
