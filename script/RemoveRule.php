2610cb645d2e7db897474ef022c8a5777e63197e
Candison November <kandisheng@163.com>
2018-02-03 22:27:34 +0800

    Add CodeMommy/DevelopPHP

--- script/RemoveRule.php
+++ script/RemoveRule.php
@@ -7,53 +7,19 @@
 
 namespace CodeMommy\Script;
 
-use DOMDocument;
+use CodeMommy\DevelopPHP\RemoveRule as Rule;
 
 /**
  * Class RemoveRule
  * @package CodeMommy\Script;
  */
-class RemoveRule
+class RemoveRule extends Rule
 {
     /**
      * RemoveRule constructor.
      */
     public function __construct()
     {
-    }
-
-    /**
-     * Get Rule To Delete
-     * @return array
-     */
-    private static function getRuleToDelete()
-    {
-        return array(
-            'cleancode' => array(
-                'StaticAccess'
-            )
-        );
-    }
-
-    /**
-     * Start
-     */
-    public static function start()
-    {
-        $ruleToDelete = self::getRuleToDelete();
-        foreach ($ruleToDelete as $fileName => $ruleName) {
-            $file = sprintf('vendor/phpmd/phpmd/src/main/resources/rulesets/%s.xml', $fileName);
-            $ruleDocument = new DOMDocument();
-            $ruleDocument->load($file);
-            $ruleSet = $ruleDocument->getElementsByTagName('rule');
-            foreach ($ruleSet as $rule) {
-                foreach ($rule->attributes as $attribute) {
-                    if ($attribute->nodeName == 'name' && in_array($attribute->nodeValue, $ruleName)) {
-                        $rule->parentNode->removeChild($rule);
-                    }
-                }
-            }
-            $ruleDocument->save($file);
-        }
+        parent::__construct();
     }
 }
